c380579ed10a62e6a83b57ccb7a1e29195ab17e1
Rudi Starcevic <ooly.me@gmail.com>
2015-07-22 10:45:21 +0800

    Add JS linting

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@
   'use strict';
 
   var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
     mocha = require('gulp-mocha'),
     exec = require('child_process').exec;
 
-  gulp.task('watch', function () {
+  gulp.task('watch-test', function () {
     gulp.watch('lib/*.js', ['test']);
+  });
+
+  gulp.task('watch-lint', function () {
+    gulp.watch('lib/*.js', ['lint']);
+  });
+
+  gulp.task('lint', function () {
+    gulp.src('lib/*.js')
+      .pipe(jshint('.jshintrc', {fail: true}))
+      .pipe(jshint.reporter()); // Console output
   });
 
   gulp.task('test', function (cb) {
